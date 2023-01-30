db20cbb873e70f8c4631121ee02a58682652858c
Jason Charney <jrcharney@gmail.com>
2023-01-29 18:24:36 -0600

    Friends. All model classes are written.

--- models/FriendList.js
+++ models/FriendList.js
@@ -8,6 +8,39 @@ import sequelize from '../config/connection.js';
 
 class FriendList extends Model {}
 
-FriendList.init();
+FriendList.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id : {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        name : {                   // Friend List Name
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 128            // Limit the length of the title to 128 charactrers
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,   // Enable timestamp fields. (I was hoping to do this anyway)
+        createdAt: true,    // Creates a createdAt field that will set a timestamp on record creation
+        updatedAt: true,    // Creates a updatedAt field that will update a timestamp on record update 
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'friend_list'
+    }
+);
 
 export default FriendList;
--- models/FriendListItem.js
+++ models/FriendListItem.js
@@ -7,6 +7,46 @@ import sequelize from '../config/connection.js';
 
 class FriendListItem extends Model {}
 
-FriendListItem.init();
+FriendListItem.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        friend_list_id : {      // Note: A friend list is associated with a user.
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        name : {                   // Friend List Name
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 128            // Limit the length of the title to 128 charactrers
+            }
+        },
+        friend_id : {           // The id of the friend not the user
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'friend',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,   // Enable timestamp fields. (I was hoping to do this anyway)
+        createdAt: true,    // Creates a createdAt field that will set a timestamp on record creation
+        updatedAt: true,    // Creates a updatedAt field that will update a timestamp on record update 
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'friend_list_item'
+    }
+);
 
 export default FriendListItem;
