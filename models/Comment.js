68bc922df80b53f80882a8732ca1312d4f82963e
Jason Charney <jrcharney@gmail.com>
2023-02-02 18:44:06 -0600

    Modify Comment model to have timestamps

--- models/Comment.js
+++ models/Comment.js
@@ -15,7 +15,7 @@ Comment.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
+        len: [5000],
       },
     },
     user_id: {
@@ -35,6 +35,9 @@ Comment.init(
   },
   {
     sequelize,
+    timestamps: true,   // Enable timestamp fields. (I was hoping to do this anyway)
+    createdAt: true,    // Creates a createdAt field that will set a timestamp on record creation
+    updatedAt: true,    // Creates a updatedAt field that will update a timestamp on record update 
     freezeTableName: true,
     underscored: true,
     modelName: "comment",
