8cd4a64dfce74c70e8c917848d596fe2d3bc0e56
Jason Charney <jrcharney@gmail.com>
2023-02-06 12:30:53 -0600

    Adding relationships to Models

--- models/index.js
+++ models/index.js
@@ -78,6 +78,64 @@ import PollItem from './PollItem.js';
 // Staff (1)
 import StaffUser from './StaffUser.js';
 
+// Relationships
+
+// User and UserProfile has a one-to-one relationship
+// User and UserSettings has a one-to-one relationship
+User.hasOne(UserProfile);
+User.hasOne(UserSettings);
+UserProfile.belongsTo(User);
+UserSettings.belongsTo(User);
+
+Author.hasMany(Book, {through: BookAuthor});
+Book.belongsToMany(Author, {through: BookAuthor});
+
+Author.hasMany(Tag, {through: AuthorTag});
+Tag.belongsToMany(Author, {through: AuthorTag});
+
+Book.hasMany(Tag, {through: BookTag});
+Tag.belongsToMany(Book, {through: BookTag});
+
+Club.hasOne(User);          // A Club has one User who is the Creator of the Club
+User.hasMany(Club);         // A User can create many Clubs
+Club.hasMany(ClubMember);
+ClubMember.belongsToMany(Club);
+ClubMember.hasOne(User);        // A ClubMember is a User
+User.hasOne(ClubMember);       // A User should only have one ClubMember associated with it (TODO: Could it have more?)
+
+Club.hasMany(Tag, {through: ClubTag});
+Tag.belongsToMany(Club, {through: ClubTag});
+
+Club.hasOne(Book, {through: BookClub});
+Book.belongsToMany(Club, {through: BookClub});      // There can be more than one club discussing the same book
+
+Club.hasOne(Author, {through: AuthorClub});
+Author.belongsToMany(Club, {through: AuthorClub});  // There can be more than one club discussing the same author
+
+Club.hasMany(Board);                // A Club has many Boards
+Board.belongsTo(Club);              // A Board belongs to a Club
+Board.belongsTo(ClubMember);        // A Board is created by a ClubMember
+ClubMember.hasMany(Board);          // A ClubMember can be part of many Boards
+Board.hasMany(Topic);               // A Board has many Topics
+Topic.belongsTo(Board);             // A Topic belongs to a Board
+Topic.belongsTo(ClubMember);        // A Topic is created by a ClubMember
+ClubMember.hasMany(Topic);          // A ClubMember can be part of many Topics
+Poll.belongsTo(Topic);              // A Poll may belong to a Topic
+Topic.hasOne(Poll);                 // A Topic can have at most one Poll
+Poll.belongsTo(ClubMember);         // A Poll is created by a ClubMember
+ClubMember.hasMany(Poll);           // A ClubMember can create many Polls
+Poll.hasMany(PollItem);             // A Poll can have many Poll Items
+PollItem.belongsTo(Poll);           // A PollItem can only belong to one Poll.
+Topic.hasMany(Post);                // A Topic can have many Posts.
+Post.belongsTo(Topic);              // A Post belongs to one topic.
+Post.belongsTo(ClubMember);         // A Post is created by a ClubMember.
+ClubMember.hasMany(Post);           // A Club Member can create many Posts.
+PostReply.belongsToMany(Post);      // A Post Reply can be in response to at least one Post
+Post.hasMany(PostReply);            // A Post can have many Post Replies
+
+
+
+
 export default { 
     User, Book, Author, Tag, Category, 
     BookAuthor , BookTag, BookCategory, AuthorTag, CategoryEchelon,
